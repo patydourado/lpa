239e2f58b8a498359468778dac832a89ec6b49a0
Patrícia Dourado <patricia.almeida@pro.ucsal.br>
2026-03-18 21:14:59 -0300

    Adicionado código básico Olá Mundo
    Código para realizar o processo de compilação via terminal

---
+++ exemplos/java/OlaMundo.java
@@ -0,0 +1,6 @@
+//Código para realizar o processo de compilação via terminal
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("Ola, alunos! Isso foi compilado via terminal.");
+    }
+}
