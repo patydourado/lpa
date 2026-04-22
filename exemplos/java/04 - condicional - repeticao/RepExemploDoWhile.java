10e7b50ede8c1128d4bf6157ff40333cdb025670
Patrícia Dourado <patricia.almeida@pro.ucsal.br>
2026-04-22 15:35:31 -0300

    Adicionado RepExemploDoWhile.java

---
+++ exemplos/java/04 - condicional - repeticao/RepExemploDoWhile.java
@@ -0,0 +1,13 @@
+package exemplo;
+
+public class RepExemploDoWhile {
+
+	public static void main(String[] args) {
+		int cont = 0;
+
+    do{
+      cont++;
+      System.out.println("Mostre" + cont);
+    }while(cont<5);
+	}
+}
